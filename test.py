1d2af681ab580309953633a29b3c6d9ceb04c92d
Bartosz Sulecki <barteksulecki@gmail.com>
2024-02-04 14:32:16 +0100

    dodaje wszystkie poprawione pliki z pythona4

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
 
+
     def test_subtract_numbers(self):
         self.assertEqual(subtract_numbers(5, 3), 2)
         self.assertEqual(subtract_numbers(-1, 1), -2)
         self.assertEqual(subtract_numbers(0, 0), 0)
 
+
     def test_multiply_numbers(self):
         self.assertEqual(multiply_numbers(5, 3), 15)
         self.assertEqual(multiply_numbers(-1, 1), -1)
         self.assertEqual(multiply_numbers(0, 5), 0)
+
 
     def test_divide_numbers(self):
         self.assertEqual(divide_numbers(6, 3), 2.0)
@@ -23,6 +26,7 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(divide_numbers(0, 5), 0.0)
         with self.assertRaises(ValueError):
             divide_numbers(5, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
